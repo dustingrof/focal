ea14cd12e97be84311fa28ebca3aa317d26b8009
Nicole MacLean <nicole.mac0404@gmail.com>
2022-05-17 18:10:21 -0700

    Clean up chat

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,5 +1,5 @@
-import { Center, Container, Button, ScrollArea, Drawer, Group, Input, Grid } from '@mantine/core';
-import { useEffect, useState} from 'react'
+import { Container, Button, ScrollArea, Drawer, Group, Input } from '@mantine/core';
+import { useEffect, useState } from 'react';
 // Socket Connection
 import io from 'socket.io-client';
 const socket = io.connect("http://localhost:3322");
@@ -8,72 +8,61 @@ const Chat = () => {
 
   const [message, setMessage] = useState("");
   const [opened, setOpened] = useState(false);
-  const [messageList, setList] = useState([{ userLS: "Dustin", message: "Hello" },  {userLS: "Iaan", message: "Hello Dustin" }]);
+  const [messageList, setList] = useState([{ userLS: "Dustin", message: "Hello" }, { userLS: "Iaan", message: "Hello Dustin" }]);
 
-  // const inputChange = (e) => {
-  //   console.log("INPUT:", e.target.value)
-  // }
-
-
+  // Gets user name from local storage for sendMessage function
   const userLS = localStorage.getItem("name");
-  const sendMessage= () => {
 
-    socket.emit('sendMessage', { message, userLS })
-  }
-  
-   useEffect(() => {
-    socket.on("receiveMessage", (data) =>{
+  // On click sends message and user to server
+  const sendMessage = () => {
+    socket.emit('sendMessage', { message, userLS });
+  };
+
+  // When message is received from server updates message list with new message
+  useEffect(() => {
+    socket.on("receiveMessage", (data) => {
       setList((prev) => {
-        return [ ...prev, data ];
-      })
-    })
-   }, [setList]);
+        return [...prev, data];
+      });
+    });
+  }, [setList]);
 
+  // Maps through messages and checks if the current user in local storage matches message user and aligns message in list.
   const messageListMapped = messageList.map((item, index) => {
-    if(item.userLS === userLS){
-       return <p key={ index + 1 } align="right">{ item.userLS }: { item.message }</p>
+    if (item.userLS === userLS) {
+      return <p key={index + 1} align="right">{item.userLS}: {item.message}</p>;
     }
-  return <p key={ index + 1 } align="left">{ item.userLS }: { item.message }</p>
-})
+    return <p key={index + 1} align="left">{item.userLS}: {item.message}</p>;
+  });
 
   return (
-   
-
-
     <>
       <Drawer
-        // opened={opened}  onClose={() => setOpened(false)}
-
         withCloseButton={false}
         opened={opened}
         onClose={() => setOpened(false)}
-        // title="Register"
         padding="xl"
         size="xl"
-        position="right"s
+        position="right" s
       >
         <Container>
-  
-      <ScrollArea style={{ height: 850 }}>
-        { messageListMapped }
-      </ScrollArea>
-        {/* <Grid.Col span={ 2 } > */}
+          <ScrollArea style={{ height: 850 }}>
+            {messageListMapped}
+          </ScrollArea>
+          {/* <Grid.Col span={ 2 } > */}
           <Input placeholder="Your message..." radius="lg" onChange={(e) => setMessage(e.target.value)} />
-          <Button onClick={ sendMessage } color="indigo" radius="md" size="xs" compact>Send</Button>
-        {/* </Grid.Col> */}
-    </Container> 
-
-
+          <Button onClick={sendMessage} color="indigo" radius="md" size="xs" compact>Send</Button>
+          {/* </Grid.Col> */}
+        </Container>
       </Drawer>
-
       <Group position="center">
         <Button onClick={() => setOpened(true)}>Open Chat</Button>
       </Group>
     </>
-  )
+  );
 
-}
+};
 export default Chat;
 
 
-        
+
