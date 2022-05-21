a3cc04c7aaa23da1615c8ca726ef6c75655b6738
Dustin Grof <dustingrof@gmail.com>
2022-05-20 23:43:51 -0700

    Create HomeView componenet and route

---
+++ client/src/components/HomeView.jsx
@@ -0,0 +1,60 @@
+import React, { useContext, useEffect } from 'react';
+import { boardContext } from '../providers/boardProvider';
+import { colourListContext } from '../providers/colourSchemeProvider';
+import { useParams, useNavigate } from 'react-router-dom';
+import '@asseinfo/react-kanban/dist/styles.css';
+
+import {
+  Space,
+  AppShell,
+  MantineProvider,
+  ColorSchemeProvider,
+} from '@mantine/core';
+
+import TopHeader from './TopHeader';
+import LeftNavbar from './LeftNavbar';
+
+export default function BoardView(props) {
+  // const navigate = useNavigate();
+
+  // const params = useParams();
+
+  // const { board, onMoveCard, setUrlBoardId, boardInfo } =
+  //   useContext(boardContext);
+
+  // useEffect(() => {
+  //   setUrlBoardId(params.board_id);
+  // }, [setUrlBoardId, params.board_id]);
+
+  const { colorScheme, setColorScheme } = useContext(colourListContext);
+  const toggleColorScheme = ColorScheme =>
+    setColorScheme(colorScheme === 'dark' ? 'light' : 'dark');
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme }}>
+        <AppShell
+          padding='md'
+          navbar={<LeftNavbar />}
+          header={<TopHeader />}
+          styles={theme => ({
+            main: {
+              backgroundColor:
+                theme.colorScheme === 'dark'
+                  ? theme.colors.dark[8]
+                  : theme.colors.gray[0],
+            },
+          })}>
+          {/* Your application here */}
+
+          <Space h='xl' />
+        </AppShell>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
+}
