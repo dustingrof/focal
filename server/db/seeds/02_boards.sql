3505a5dbce440c2abb615357812405075acad05b
Nicole MacLean <nicole.mac0404@gmail.com>
2022-05-22 20:34:00 -0700

    Board seeds sequence key change

--- server/db/seeds/02_boards.sql
+++ server/db/seeds/02_boards.sql
@@ -2,6 +2,6 @@ insert into boards (id, name, description, image_url, created_at, active) values
 insert into boards (id, name, description, image_url, created_at, active) values (2, 'Birdwatching App', 'Build an app for bird watching', 'https://upload.wikimedia.org/wikipedia/commons/a/a7/Northern_shoveler_Steve_Sinclair_outreach_use_only_%2819838806616%29.jpg', '2022-05-12', true);
 insert into boards (id, name, description, image_url, created_at, active) values (3, 'Homebrew App', 'Build an app for managing home-brewing', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/beer-surrounded-by-hops-on-wooden-background-royalty-free-image-1611158342.?crop=0.669xw:1.00xh;0.0179xw,0&resize=640:*', '2022-05-12', true);
 insert into boards (id, name, description, image_url, created_at, active) values (4, 'Ski Tour App', 'Build an app to track ski touring trip reports', 'https://t4.ftcdn.net/jpg/04/01/44/19/360_F_401441954_1mr208aUSzRzkL6Nbw3f8cJ9ueVkKxLC.jpg', '2022-05-12', true);
-ALTER SEQUENCE boards_id_seq RESTART WITH 2;
+ALTER SEQUENCE boards_id_seq RESTART WITH 5;
 
 
