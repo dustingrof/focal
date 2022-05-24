1d49751131f051c3dc816820443d414afbf8a7e4
Dustin Grof <dustingrof@gmail.com>
2022-05-24 00:22:18 -0700

    Tweak server production build

--- server/server.js
+++ server/server.js
@@ -153,9 +153,9 @@ io.on('connection', socket => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
   });
 }
 
